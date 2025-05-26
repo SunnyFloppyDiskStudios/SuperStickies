f117bd9c417c8b3b23c6ba030c369abc10b4962e
SunnyFlops <arav.prasad@outlook.com>
2025-05-27 10:03:04 +1200

    initial commit

---
+++ SuperStickies/SuperStickiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperStickiesApp.swift
+//  SuperStickies
+//
+//  Created by SunnyFlops on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SuperStickiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
