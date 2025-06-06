79412df9c6de444c83602a5623663089a9deaf45
SunnyFlops <arav.prasad@outlook.com>
2025-06-06 12:19:28 +1200

    Update SaveDataManager.swift

--- SuperStickies/Components/SaveDataManager.swift
+++ SuperStickies/Components/SaveDataManager.swift
@@ -7,36 +7,5 @@
 
 import Foundation
 import SwiftUI
-
-struct StickyNote {
-    var id: UUID
-    var note: NSAttributedString
-    var colour: Color
-    var screenPos: String
-}
-
-func saveData(id: UUID, note: String, colour: Color) {
-    let fileManager = FileManager.default
-    
-    let dd = URL.documentsDirectory
-    let saveDataLocation = dd.appendingPathComponent("SuperStickies", isDirectory: true)
-    
-    try? fileManager.createDirectory(at: saveDataLocation, withIntermediateDirectories: false)
-    
-    let fileURL = saveDataLocation.appendingPathComponent("\(id).rtf")
-
-    try? Data(note.utf8).write(to: fileURL)
-}
-
-func loadData() {
-    let fileManager = FileManager.default
-    
-    let dd = URL.documentsDirectory
-    let saveDataLocation = dd.appendingPathComponent("SuperStickies", isDirectory: true)
-    
-    let items = try! fileManager.contentsOfDirectory(at: saveDataLocation, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-    
-    for file in items {
-        
-    }
-}
+import Combine
+import AppKit
